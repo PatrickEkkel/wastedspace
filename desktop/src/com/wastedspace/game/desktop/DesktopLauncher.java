5753840a2d93b22f5f05a284a6986711dcffe71e
patrick <patrickekkel@gmail.com>
2015-02-03 23:58:04 +0100

    libGDX project setup

---
+++ desktop/src/com/wastedspace/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.wastedspace.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.wastedspace.game.MainGdx;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGdx(), config);
+	}
+}
